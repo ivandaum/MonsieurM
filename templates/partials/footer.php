ff1eac0b2aef20eaaa0793d3494fb27a20d689bc
Ivan Daum <ivan.daum@gmail.com>
2020-05-30 10:50:27 +0200

    style(footer): full height on home

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -4,7 +4,7 @@
 
     $email = get_field('config__email', 'options');
 ?>
-<footer class="Footer is-relative container">
+<footer class="Footer is-flex is-right-y is-relative container">
     <ul class="is-flex is-justified-x is-relative has-width-100">
         <li><a class="is-h5" href="mailto:<?= $email ?>"><?= $email ?></a></li>
         <li><button class="js-scroll-top is-h6 has-font-title with-spacing is-uppercase has-text-light">Top</button></li>
